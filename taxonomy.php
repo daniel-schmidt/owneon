752f4c01099c1250d35b669506c996e8c299b753
Daniel Schmidt <daniel@neonfoto.de>
2015-11-30 22:08:56 +0100

    Started work on new main gallery. Included code to show all gallery categories in order to filter images."

--- taxonomy.php
+++ taxonomy.php
@@ -18,7 +18,7 @@
             $postID = get_the_ID();
             if ( wp_attachment_is_image( $postID ) ) :
             $gallery_shortcode = $gallery_shortcode . $postID  . ', '; // intval( $post->post_parent )
-// 	      echo $gallery_shortcode;
+//             echo $gallery_shortcode;
 // 	      the_attachment_link( postID, false, false, true );
             else :
             echo get_the_excerpt();
@@ -29,9 +29,29 @@
         endwhile; // the Loop
         endif; // end have_posts
         $gallery_shortcode = substr( $gallery_shortcode, 0, -2); // removing last colon
-            $gallery_shortcode = $gallery_shortcode . '" rows="4" cols="4"]';
+        $gallery_shortcode = $gallery_shortcode . '" rows="4" cols="5"]';
 // 	$gallery_shortcode = $gallery_shortcode . '" columns="5"]';	
-// 	      echo $gallery_shortcode;
+        echo $gallery_shortcode;
+        
+        // get all taxonomy terms to display buttons
+        $terms = get_terms( 'galerie_kategorie' );
+        if ( ! empty( $terms ) && ! is_wp_error( $terms ) ) {
+            $count = count( $terms );
+            $i = 0;
+            $term_list = '<p class="my_term-archive">';
+            foreach ( $terms as $term ) {
+                $i++;
+                $term_list .= '<a href="' . esc_url( get_term_link( $term ) ) . '" alt="' . esc_attr( sprintf( __( 'View all post filed under %s', 'my_localization_domain' ), $term->name ) ) . '">' . $term->name . '</a>';
+                if ( $count != $i ) {
+                    $term_list .= ' &middot; ';
+                }
+                else {
+                    $term_list .= '</p>';
+                }
+            }
+            echo $term_list;
+        }
+        
         remove_filter( 'the_content', 'prepend_attachment');
         echo apply_filters( 'the_content', $gallery_shortcode );
         ?>
