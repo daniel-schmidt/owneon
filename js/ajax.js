8f078d39d223b0698b3f316bb543085c0109c312
Daniel Schmidt <daniel@neonfoto.de>
2016-05-10 23:20:16 +0200

    AJAX animations, sliding in from left.

--- js/ajax.js
+++ js/ajax.js
@@ -1,38 +1,21 @@
 jQuery( document ).ready( function($) {
-    console.log( 'Hallo Ajax!');
-//     $( 'a.gallery-link' ).on( 'click', function( e ) {
-    $( 'body' ).on( 'click', 'a.gallery-link', function( e ) {
-        e.preventDefault();
-        var url = this.href;
-        
-        $( '.gallery-curr-item' ).removeClass( 'gallery-curr-item' );
-        $( this ).addClass( 'gallery-curr-item' );
-
-        
-        $( '#galerie .content-container' ).remove();
-        // TODO: this works only if we have only one galerie
-        $( '#galerie .content-area' ).load( url + ' #galerie .content-container' ).hide().fadeIn('slow');
-        $.scrollTo('#galerie', 800);
-    } );
     
-    $( 'body' ).on( 'click', 'a.blog-link', function( e ) {
-        e.preventDefault();
-        var url = this.href;
-        
-        $( '#blog .content-container' ).remove();
-        // TODO: this works only if we have only one galerie
-        $( '#blog .content-area' ).load( url + ' #blog .content-container' ).hide().fadeIn('slow');
-        $.scrollTo('#blog', 800);
-    } );
-    
-    $( 'body' ).on( 'click', '#primary-menu a', function( e ) {
+    $( 'body' ).on( 'click', '#primary-menu a, a.blog-link, a.gallery-link', function( e ) {
         e.preventDefault();
         var url = this.href;
         var hash = this.hash;
-        console.log(hash);
         $.scrollTo( hash, 800 );
-        $( hash + ' .content-container' ).remove();
-        $( hash + ' .content-area' ).load( url + ' ' + hash + ' .content-container' ).hide().fadeIn('slow');
+        $( hash + ' .content-container' )
+//         .animate({'margin-left': '+=2000'}, 800)
+        .remove();
+
+        $( hash + ' .content-area' ).load( url + ' ' + hash + ' .content-container' )
+        .hide()
+//         .fadeIn('slow')
+        .css('margin-left', '-2000px')
+        .show()
+        .animate({'margin-left': '+=2000'},800)
+        ;
     } );
     
 } );
