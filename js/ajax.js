359025a59d87585203201e7a36f3a3c19ec9b31d
Daniel Schmidt <daniel@neonfoto.de>
2016-05-21 18:17:11 +0200

    Ajax basics working.

--- js/ajax.js
+++ js/ajax.js
@@ -1,10 +1,10 @@
 jQuery( document ).ready( function($) {
     
-    $( 'body' ).on( 'click', '#primary-menu a, a.blog-link, a.gallery-link', function( e ) {
+    $( 'body' ).on( 'click', '#primary-menu a, a.blog-link, a.gallery-link, a.info-link', function( e ) {
         e.preventDefault();
         var url = this.href;
         var hash = this.hash;
-        $.scrollTo( hash, 800 );
+        $.scrollTo( hash, 1000 );
         $( hash + ' .content-container' )
 //         .animate({'margin-left': '+=2000'}, 800)
         .remove();
@@ -14,7 +14,7 @@ jQuery( document ).ready( function($) {
 //         .fadeIn('slow')
         .css('margin-left', '-2000px')
         .show()
-        .animate({'margin-left': '+=2000'},800)
+        .animate({'margin-left': '+=2000'},500)
         ;
     } );
     
--- template-parts/page-viewer.php
+++ template-parts/page-viewer.php
@@ -8,7 +8,7 @@
         <div id="<?php echo $page->post_name; ?>" class="section">
             <header class="section-header">
                 <h1 class="section-heading">
-                    <a href=" <?php echo get_page_link( $page->ID ) ?> ">
+                    <a class="info-link" href=" <?php echo get_page_link( $page->ID ) ?> ">
                         <?php echo $page->post_title; ?>
                     </a>
                 </h1>
@@ -19,7 +19,7 @@
                     ) );
                 $menu_items='<nav class="section-menu-container"><ul class="section-menu">';
                 foreach( $sub_pages as $sub_page ) {
-                    $menu_items .= '<li class="foreground section-menu-item"><a href="' . get_page_link( $sub_page->ID ) . '">';
+                    $menu_items .= '<li class="foreground section-menu-item"><a class="info-link" href="' . get_page_link( $sub_page->ID ) . '">';
                     $menu_items .= $sub_page->post_title;
                     $menu_items .= '</li></a>';
                 }
@@ -27,43 +27,45 @@
                 echo $menu_items; ?>
             </header>
             <div class="content-area content-centered fixed-width">
-                <?php 
-                $count = 0;
-                    if( have_posts() ) :
-                        while ( have_posts() ) :
-                            the_post();
-                            
-                            // if we are displaying a page, show it in the correct section
-                            if( is_page() 
-                                && !is_front_page()
-                                && get_the_ID() == $page->ID
-                                || in_array( $page->ID, get_post_ancestors( get_the_ID() ) ) ) :
+                <div class="content-container">
+                    <?php 
+                    $count = 0;
+                        if( have_posts() ) :
+                            while ( have_posts() ) :
+                                the_post();
                                 
-                                get_template_part( 'template-parts/content', 'page' );
-                            elseif( $count == 0 ) :
-                                // we are not active for the current post, so we display the main page, but only once ?>
-                                <article id="post-<?php $page->ID; ?>" <?php post_class( $page->ID ); ?>>
-                                    <header class="entry-header">
-                                            <h1 class="entry-title"><?php echo $page->post_title; ?></h1>
-                                    </header><!-- .entry-header -->
+                                // if we are displaying a page, show it in the correct section
+                                if( is_page() 
+                                    && !is_front_page()
+                                    && get_the_ID() == $page->ID
+                                    || in_array( $page->ID, get_post_ancestors( get_the_ID() ) ) ) :
+                                    
+                                    get_template_part( 'template-parts/content', 'page' );
+                                elseif( $count == 0 ) :
+                                    // we are not active for the current post, so we display the main page, but only once ?>
+                                    <article id="post-<?php $page->ID; ?>" <?php post_class( $page->ID ); ?>>
+                                        <header class="entry-header">
+                                                <h1 class="entry-title"><?php echo $page->post_title; ?></h1>
+                                        </header><!-- .entry-header -->
 
-                                    <div class="entry-content">
-                                            <?php echo $page->post_content; ?>
-                                            <?php
-                                                    wp_link_pages( array(
-                                                            'before' => '<div class="page-links">' . __( 'Pages:', 'owneon' ),
-                                                            'after'  => '</div>',
-                                                    ) );
-                                            ?>
-                                    </div><!-- .entry-content -->
-                                    <?php edit_post_link( __( 'Edit', 'owneon' ), '<footer class="entry-meta"><span class="edit-link">', '</span></footer>' ); ?>
-                                </article><!-- #post-## -->
-                            <?php 
-                            $count++;
-                            endif;
-                        endwhile; // end of the loop.    
-                    rewind_posts();
-                endif; ?>
+                                        <div class="entry-content">
+                                                <?php echo $page->post_content; ?>
+                                                <?php
+                                                        wp_link_pages( array(
+                                                                'before' => '<div class="page-links">' . __( 'Pages:', 'owneon' ),
+                                                                'after'  => '</div>',
+                                                        ) );
+                                                ?>
+                                        </div><!-- .entry-content -->
+                                        <?php edit_post_link( __( 'Edit', 'owneon' ), '<footer class="entry-meta"><span class="edit-link">', '</span></footer>' ); ?>
+                                    </article><!-- #post-## -->
+                                <?php 
+                                $count++;
+                                endif;
+                            endwhile; // end of the loop.    
+                        rewind_posts();
+                    endif; ?>
+                </div> <!-- content-container -->
             </div> <!-- content-area -->
         </div> <!-- section -->
     <?php endforeach;
