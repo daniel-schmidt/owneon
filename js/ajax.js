3d8e8a56071b6e5d8a39b9be1d4575f8d651f96c
Daniel Schmidt <daniel@neonfoto.de>
2016-08-14 16:43:47 +0200

    Updating address bar and history.

--- js/ajax.js
+++ js/ajax.js
@@ -1,14 +1,10 @@
 jQuery( document ).ready( function($) {
     
-    $( 'body' ).on( 'click', '#primary-menu a, a.blog-link, a.gallery-link, a.info-link', function( e ) {
-        e.preventDefault();
-        var url = this.href;
-        var hash = this.hash;
-        $.scrollTo( hash, 1000 );
+    function loadContent( url, hash ) {
         $( hash + ' .content-container' )
 //         .animate({'margin-left': '+=2000'}, 800)
         .remove();
-
+        
         $( hash + ' .content-area' ).load( url + ' ' + hash + ' .content-container' )
         .hide()
 //         .fadeIn('slow')
@@ -16,6 +12,25 @@ jQuery( document ).ready( function($) {
         .show()
         .animate({'margin-left': '+=2000'},500)
         ;
+    };
+    
+    $( 'body' ).on( 'click', '#primary-menu a, a.blog-link, a.gallery-link, a.info-link', function( e ) {
+        e.preventDefault();
+        var url = this.href;
+        var hash = this.hash;
+        $.scrollTo( hash, 1000 );
+        var $this = $(this);
+     
+        loadContent( url, hash );
+        
+        history.pushState('', $this.text, url );
     } );
+    
+    window.onpopstate = function() {
+        var url = location.pathname;
+        var hash = location.hash;
+//         console.log( url, hash );
+        loadContent( url, hash );
+    };
     
 } );
